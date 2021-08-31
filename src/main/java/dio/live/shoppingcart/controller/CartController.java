ef0f25ca690ef1e5634ccef0af8c506ecc7ab11e
rogs <pilique11@gmail.com>
2021-08-31 20:43:57 -0300

    POST cart ok

--- src/main/java/dio/live/shoppingcart/controller/CartController.java
+++ src/main/java/dio/live/shoppingcart/controller/CartController.java
@@ -24,5 +24,23 @@ public class CartController {
     public Optional<Cart> findById(@PathVariable("id") Integer id){ return cartRepository.findById(id); }
 
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public Cart addItem(@PathVariable("id") Integer id, @RequestBody Item item){
+
+        Optional<Cart> savedCart = cartRepository.findById(id);
+        Cart cart;
+        if(savedCart.equals(Optional.empty())){
+            cart = new Cart(id);
+        }
+        else{
+            cart = savedCart.get();
+        }
+
+        cart.getItems().add(item);
+        return cartRepository.save(cart);
+
+    }
+
+
 
 }
